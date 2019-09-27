d0b32b17dbd4f8238a78fb38b32de6d1d696176b
Dmitry Zhukov <zhukov.dima.40@gmail.com>
2019-09-27 22:11:03 +0300

    articles mock  was added

---
+++ src/app/mock/articles.ts
@@ -0,0 +1,33 @@
+import { Article } from '../types/article';
+
+
+export const ARTICLES: Article[] = [
+    {
+        id: 1,
+        title: 'Волнетрство в детском доме',
+        body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam magni accusantium sapiente reprehenderit, dicta debitis quam dolore consectetur minima eaque expedita harum aut obcaecati id nobis aperiam distinctio repellat. Enim nemo officia esse minus odit. Obcaecati similique modi officia saepe!',
+        date: new Date(),
+        author: 'Иванов Иван',
+        rating: 1000,
+        community: 'Молодая гвардия'
+    },
+    {
+        id: 2,
+        title: 'Волнетрство в доме пристарелых',
+        body: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores sint neque maxime nobis harum eum corrupti odio hic deserunt possimus, dolore sunt. Fugit ipsum similique sequi, quidem modi dolorum recusandae?',
+        date: new Date(),
+        author: 'Иванов Иван',
+        rating: 200,
+        community: 'Молодая гвардия'
+    },
+    {
+        id: 3,
+        title: 'Поискова операция пропавшего в лесу',
+        body: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores sint neque maxime nobis harum eum corrupti odio hic deserunt possimus, dolore sunt. Fugit ipsum similique sequi, quidem modi dolorum recusandae?',
+        date: new Date(),
+        author: 'Петров Петр',
+        rating: 300,
+        community: 'Лиза алерт'
+    },
+];
+
