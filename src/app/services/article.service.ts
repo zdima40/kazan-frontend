3cdd95d2d9fbe2fc460e7e8c7efd502c3d8e3a75
Farlaff <Fsarlaff2@gmail.com>
2019-09-28 14:58:36 +0300

    Article creation prototype was added

--- src/app/services/article.service.ts
+++ src/app/services/article.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Article } from '@classtypes/article';
 import { ARTICLES } from '../mocks/articles';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class ArticleService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   public getArticles(): Article[] {
     return ARTICLES;
@@ -17,4 +19,9 @@ export class ArticleService {
     const articles: Article[] =  this.getArticles();
     return articles.filter(article => article.title.includes(text));
   }
+
+
+  createArticle(article: Article): Observable<{id: number}> {
+    return this.http.post<{id: number}>('http://127.0.0.1/api/article/create', article);
+}
 }
