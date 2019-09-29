1774fd146c6a8c71beb59a81acecc84492581159
Farlaff <Fsarlaff2@gmail.com>
2019-09-29 09:27:44 +0300

    header was added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ArticlesComponent } from './components/articles/articles.component';
 import { GrantsComponent } from './components/grants/grants.component';
-import { PartnersListComponent } from './components/partners-list/partners-list.component';
 import { CreateArticleComponent } from './components/create-article/create-article.component';
 
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: ArticlesComponent },
-  { path: 'fresh', component: GrantsComponent },
-  { path: 'partners-list', component: PartnersListComponent },
   { path: 'create-article', component: CreateArticleComponent },
- // { path: 'profile', component: ProfileComponent },
+  { path: 'fresh', component: ArticlesComponent },
+  
 ];
 
 @NgModule({
