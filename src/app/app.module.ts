1076323e78dbbaf3ea57e8e994ffef65c8e237ab
Dmitry Zhukov <zhukov.dima.40@gmail.com>
2019-09-29 05:45:30 +0300

    ages was added to assets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatRippleModule } from '@angular/material/core';
@@ -13,16 +11,19 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
+import { HttpClientModule } from '@angular/common/http';
 
+import { AppComponent } from './app.component';
 import { ArticlesComponent } from './components/articles/articles.component';
 import { ArticleComponent } from './components/articles/article/article.component';
-
-import { ArticleService } from './services/article.service';
 import { HeaderComponent } from './components/header/header.component';
 import { GrantsComponent } from './components/grants/grants.component';
 import { CreateArticleComponent } from './components/create-article/create-article.component';
+import { PartnersListComponent } from './components/partners-list/partners-list.component';
+
+import { ArticleService } from './services/article.service';
 import { CreateArticleService } from '@services/create-article.service';
-import { HttpClientModule } from '@angular/common/http';
+import { PartnerCardService } from '@services/partner-card.service';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import { HttpClientModule } from '@angular/common/http';
     ArticleComponent,
     HeaderComponent,
     GrantsComponent,
-    CreateArticleComponent
+    CreateArticleComponent,
+    PartnersListComponent
   ],
   imports: [
     BrowserModule,
@@ -51,7 +53,8 @@ import { HttpClientModule } from '@angular/common/http';
   ],
   providers: [
     ArticleService,
-    CreateArticleService
+    CreateArticleService,
+    PartnerCardService
   ],
   bootstrap: [AppComponent]
 })
